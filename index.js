7e3b0fce13112ab0f42305f9c52cc3dd23c8c83f
YADA <624235051@parichat.skru.ac.th>
2021-08-19 19:45:42 +0700

    add student add circle

--- index.js
+++ index.js
@@ -38,6 +38,28 @@ app.get('/books',  function (req, res)  {
   
 });
 
+app.get('/student/:studentId',  function (req, res)  {  
+
+	res.setHeader('Content-Type', 'application/json');
+	var studentId = req.params.studentId;
+
+	var booksReference = db.ref("students");
+
+	//Attach an asynchronous callback to read the data
+	booksReference.orderByChild("studentId").equalTo(studentId).on("child_added", 
+				function(snapshot) {					
+					res.json(snapshot.val());
+					booksReference.off("value");
+					}, 
+				function (errorObject) {
+					res.send("The read failed: " + errorObject.code);
+				});
+  
+});
+
+
+
+
 
 app.get('/topsellers',  function (req, res)  {  
 
@@ -64,6 +86,15 @@ app.get('/book/:bookid',  function (req, res)  {
 
 });
 
+app.post('/rectangle',function(req,res){
+	res.setHeader('Content-Type', 'application/json');
+
+	var width = req.body.width;
+	var long = req.body.long;
+
+	res.send('{"area": '+(width*long)+'}');
+});
+
 app.delete('/book/:bookid',  function (req, res)  {  
   	
 	//Code Here
@@ -72,6 +103,13 @@ app.delete('/book/:bookid',  function (req, res)  {
 
 });
 
+app.post('/circle',function(req,res){
+	res.setHeader('Content-Type', 'application/json');
+
+	var radius = req.body.radius;
+
+	res.send('{"area": '+ (radius*radius*3.14)+'}');
+});
 
 app.get('/lastorderid',  function (req, res)  {  
   	
@@ -111,3 +149,4 @@ app.post('/order',  function (req, res)  {
 app.listen(port, function () {
     console.log("Server is up and running...");
 });
+
