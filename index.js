b5805d46469be63bf9b1c2ccb5b2db95a16dded8
YADA <624235051@parichat.skru.ac.th>
2021-08-22 12:21:41 +0700

    add Post  add Delete

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/books', function (req, res) {
 
 });
 
-app.get('/student/:studentId', function (req, res) {
+app.get('/student/:studentId',  function (req, res)  {  
 
 	res.setHeader('Content-Type', 'application/json');
 	var studentId = req.params.studentId;
@@ -46,16 +46,60 @@ app.get('/student/:studentId', function (req, res) {
 	var booksReference = db.ref("students");
 
 	//Attach an asynchronous callback to read the data
-	booksReference.orderByChild("studentId").equalTo(studentId).on("child_added",
-		function (snapshot) {
-			res.json(snapshot.val());
-			booksReference.off("value");
-		},
-		function (errorObject) {
-			res.send("The read failed: " + errorObject.code);
-		});
+	booksReference.orderByChild("studentId").equalTo(studentId).on("child_added", 
+				function(snapshot) {					
+					res.json(snapshot.val());
+					booksReference.off("value");
+					}, 
+				function (errorObject) {
+					res.send("The read failed: " + errorObject.code);
+				});
 
 });
+
+
+app.post('/student', function (req, res) {
+	var students = Number(req.body.students);
+	var studentId = req.body.studentId;
+	var studentName = req.body.studentName;
+
+	var referencePath = '/students/' + students + '/';
+	var studentsReference = db.ref(referencePath);
+
+
+	if (studentsReference != null) {
+
+		studentsReference.update({ studentId:studentId, studentName:studentName },
+			function (error) {
+				if (error) {
+					res.send("Data could not be saved." + error)
+				}
+				else {
+					res.send("Success!!");
+				}
+
+			}
+
+		);
+	}
+
+});
+
+app.delete('/student/:students', function (req, res){
+	
+	var students = req.params.students;
+
+	var referencePath = '/students/' + students + '/';
+	var studentsReference = db.ref(referencePath);
+	if (studentsReference != null){
+		studentsReference.remove()
+		res.send("Succrss!!")
+	}
+	//if (error) throw error;
+
+});
+
+
 
 app.post('/book', function (req, res) {
 
